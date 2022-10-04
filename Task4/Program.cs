812359056f6d581055fd9e082204d2f1a6312030
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-04 14:34:37 +0300

    Task4 completed

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Count = 1;
+
+if (N < Count)
+{
+    Console.WriteLine("Введите положительное число которое больше нуля");
+}
+
+while(Count <= N)
+{
+    if (Count%2 == 0) {Console.Write(Count + " ");}
+    Count++;
+}
