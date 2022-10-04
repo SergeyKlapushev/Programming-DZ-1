0009ea4e08bd5713c80ec8866f236f82208ef661
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-04 13:32:45 +0300

    Task2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Введите три числа: ");
+
+int [] array = {Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
+
+int maxCount = 0;
+
+for(int i =0; i < array.Length; i++)
+{
+    if(array[i] > maxCount)
+    {
+        maxCount = array[i];
+    }
+}
+
+Console.WriteLine($"Максимальное число: {maxCount}");
