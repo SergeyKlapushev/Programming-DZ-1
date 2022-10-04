faf65bfd6cef923095d6cc941c79c708b91d2cb0
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-04 13:44:44 +0300

    Task3 copleted

---
+++ Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int ost = n%2;
+Console.WriteLine(ost);
+if (ost > 0 || ost < 0) {Console.Write($"{n} —> нечётное");}
+else {Console.Write($"{n} —> чётное");}
