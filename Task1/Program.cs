147d318a0597b9e899e691e2d93b9f2b01126539
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-04 14:42:31 +0300

    Change code

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,6 +4,6 @@ int n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int n2 = Convert.ToInt32(Console.ReadLine());
 
-if (n1 > n2) { Console.WriteLine("Первое число больше чем второе"); }
-if (n2 > n1) { Console.WriteLine("Второе число больше чем первое"); }
-if (n2 == n1) { Console.WriteLine("Оба числа равны"); }
+if (n1 > n2) { Console.WriteLine($"{n1} больше {n2}"); }
+if (n2 > n1) { Console.WriteLine($"{n2} больше {n1}"); }
+if (n2 == n1) { Console.WriteLine($"{n1} и {n2} равны"); }
